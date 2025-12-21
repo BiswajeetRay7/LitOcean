8de35b700633b53f3155264664df81711b4ebd73
Biswajeet Ray <94063930+BiswajeetRay7@users.noreply.github.com>
2025-12-21 20:20:11 +0530

    Update sources_tools.go

--- sources_tools.go
+++ sources_tools.go
@@ -11,11 +11,16 @@ func RunTool(name string, args []string, p *tea.Program) {
 	update(p, name, 0, "Running")
 
 	cmd := exec.Command(name, args...)
-	out, _ := cmd.StdoutPipe()
-	cmd.Start()
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		update(p, name, 0, "Error")
+		return
+	}
 
+	cmd.Start()
 	sc := bufio.NewScanner(out)
 	count := 0
+
 	for sc.Scan() {
 		addSub(sc.Text())
 		count++
