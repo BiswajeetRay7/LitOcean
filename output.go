3465580e4be0b021d1c06e6d3560b92a758bf441
Biswajeet Ray <94063930+BiswajeetRay7@users.noreply.github.com>
2025-12-21 16:35:09 +0530

    Create output.go

---
+++ output.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"sort"
+)
+
+func saveSubs(file string) {
+	resultsMu.Lock()
+	defer resultsMu.Unlock()
+
+	var subs []string
+	for s := range results {
+		subs = append(subs, s)
+	}
+	sort.Strings(subs)
+
+	f, _ := os.Create(file)
+	defer f.Close()
+	for _, s := range subs {
+		f.WriteString(s + "\n")
+	}
+}
+
+func exportAll() {
+	prev := loadPrevious("subs.txt")
+	saveSubs("subs.txt")
+	saveDiff(prev, results)
+	ensureHTTPX()
+	exec.Command(
+		"httpx",
+		"-l", "subs.txt",
+		"-silent",
+		"-sc",
+		"-title",
+		"-o", "alive.txt",
+	).Run()
+}
